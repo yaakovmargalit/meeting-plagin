35ade18cc2cda7546ac772072583abd6b4e2e82e
Yaakov Margalit <yaakovmargalit@gmail.com>
2023-04-23 00:09:24 +0300

    dgfdxsf

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'abe04da715303b8b0226');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '4d3212d2e75401cd45de');
