b86e30c4469afc7362e4d85a705f4a7ed5f26aef
Yaakov <yaakovmargalit@gmail.com>
2023-05-13 22:46:08 +0300

    ygujh

--- src/admin-page/components/Settings.js
+++ src/admin-page/components/Settings.js
@@ -1,26 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import DaysSettings from './DaysSettings';
-import { getMeetingSettings } from '../service'
+import { getMeetingSettings, removeAvailability } from '../service'
 import { addNewAvailability } from '../service'
 const Settings = () => {
-    const [meetingSettings, setMeetingSettings] = useState(null)
-    const [update, setUpdate] = useState(false);
+  const [meetingSettings, setMeetingSettings] = useState(null)
+  const [update, setUpdate] = useState(false);
 
-    useEffect(async () => {
-        const settings = await getMeetingSettings()
-        setMeetingSettings(settings)
-    }, [])
+  useEffect(async () => {
+    const settings = await getMeetingSettings()
+    setMeetingSettings(settings)
+  }, [])
 
-   async function updateData(startTime, endTime, meetingLength,selctedDayCode){
-      await  addNewAvailability(startTime, endTime, meetingLength,selctedDayCode)
-      const settings = await getMeetingSettings()
-      setMeetingSettings(settings)    }
+  async function updateData(startTime, endTime, meetingLength, selctedDayCode) {
+    await addNewAvailability(startTime, endTime, meetingLength, selctedDayCode)
+    const settings = await getMeetingSettings()
+    setMeetingSettings(settings)
+  }
 
-    return (
-        <React.Fragment>
-          { meetingSettings && <DaysSettings addNewAvailability={updateData} availabilitys={meetingSettings.availabilitys} />}
-        </React.Fragment>
-    )
+  async function deleteAvailability(id){
+    await removeAvailability(id)
+    const settings = await getMeetingSettings()
+    setMeetingSettings(settings)
+  }
+
+  return (
+    <React.Fragment>
+      {meetingSettings && <DaysSettings deleteAvailability={deleteAvailability} addNewAvailability={updateData} availabilitys={meetingSettings.availabilitys} />}
+    </React.Fragment>
+  )
 }
 
 export default Settings;
